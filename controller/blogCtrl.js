cd17ca7e506431d56e14a15193fecd73cb22ea9e
ismailahammed1 <ismailahmmedroman@gmail.com>
2023-08-28 02:04:37 +0600

    blog update, get all blog, get a blog

--- controller/blogCtrl.js
+++ controller/blogCtrl.js
@@ -1,18 +1,69 @@
-const User = require('../models/UserModels')
-const Blog=require("../models/BlogModel");
-const asyncHandler= require('express-async-handler')
+const User = require("../models/UserModels");
+const Blog = require("../models/BlogModel");
+const asyncHandler = require("express-async-handler");
 const validateMongoDbId = require("../utils/validateMongoDbId");
 
-const createBlog= asyncHandler(async(req, res)=>{
-    try {
-        const newBlog = await Blog.create(req.body);
-        res.json({
-            newBlog
-        })
-    } catch (error) {
-        throw new Error(error)
-    }
+const createBlog = asyncHandler(async (req, res) => {
+  try {
+    const newBlog = await Blog.create(req.body);
+    res.json({
+      newBlog,
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
 });
 
+// updateBlog
+const updateBlog = asyncHandler(async (req, res) => {
+  const id = req.params.id; // Extract the id from request parameters
 
-module.exports={createBlog}
+  try {
+    const newupdateBlog = await Blog.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+
+    res.json({
+      newupdateBlog,
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+// get a blog
+const getBlog = asyncHandler(async (req, res) => {
+    const id = req.params.id; // Extract the id from request parameters
+  
+    try {
+      const blog = await Blog.findById(id);
+      if (!blog) {
+        return res.status(404).json({ message: "Blog not found" });
+      }
+      const updatedBlog = await Blog.findByIdAndUpdate(
+        id,
+        { $inc: { numViews: 1 } },
+        {
+          new: true,
+        }
+      );
+      res.json({
+        blog: updatedBlog,
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
+  });
+
+  ///?get all Blog
+const getAllBlog = asyncHandler(async (req, res) => {
+    try {
+      const getAllBlog = await Blog.find();
+      res.json({
+        getAllBlog
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
+  });
+
+module.exports = { createBlog, updateBlog,getBlog,getAllBlog };
--- routes/BlogRoutes.js
+++ routes/BlogRoutes.js
@@ -1,9 +1,13 @@
-const express=require('express');
-const { createBlog } = require('../controller/blogCtrl');
+// routes/blogRoutes.js
+const express = require("express");
+const { createBlog, updateBlog, getBlog, getAllBlog } = require("../controller/blogCtrl");
 const { authMiddleware, isAdmin } = require('../middlewares/autMiddlewares');
 
-const router= express.Router();
+const router = express.Router();
 
-router.post('/', authMiddleware,isAdmin, createBlog)
+router.post('/', authMiddleware, isAdmin, createBlog);
+router.put('/:id', authMiddleware, isAdmin, updateBlog);
+router.get('/:id', getBlog);
+router.get('/', getAllBlog); 
 
 module.exports = router;
