6458cc8bb2096ac93830c0d5333c81d7f4f3b420
ismailahammed1 <ismailahmmedroman@gmail.com>
2023-09-10 05:09:27 +0600

    create get one coupon

--- routes/CouponRoute.js
+++ routes/CouponRoute.js
@@ -1,11 +1,12 @@
 const express=require("express")
 const { authMiddleware, isAdmin } = require('../middlewares/autMiddlewares');
-const { createCoupon, getALLCoupon } = require("../controller/CouponCtrl");
+const { createCoupon, getALLCoupon, getACoupon } = require("../controller/CouponCtrl");
 
 
 const router=express.Router();
 
 router.post('/',authMiddleware,isAdmin,createCoupon) 
 router.get('/',authMiddleware,isAdmin,getALLCoupon) 
+router.get('/:id',authMiddleware,isAdmin,getACoupon) 
 
 module.exports=router
