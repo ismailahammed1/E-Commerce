b36bcbb276147dc9cc939cf88b6b0acb606b79b5
ismailahammed1 <ismailahmmedroman@gmail.com>
2023-09-10 05:21:21 +0600

    updated coupon

--- controller/CouponCtrl.js
+++ controller/CouponCtrl.js
@@ -28,6 +28,24 @@ const getACoupon = asyncHandler(async (req, res) => {
       throw new Error(error);
     }
   });
+const updateCoupon = asyncHandler(async (req, res) => {
+    try {
+      const {id}=req.params
+      const updateCoupon = await Coupon.findByIdAndUpdate(id, req.body, {new:true});
+      res.status(201).json(updateCoupon);
+    } catch (error) {
+      throw new Error(error);
+    }
+  });
+const deleteCoupon = asyncHandler(async (req, res) => {
+    try {
+      const {id}=req.params
+      const deleteCoupon = await Coupon.findByIdAndDelete(id);
+      res.status(201).json(deleteCoupon);
+    } catch (error) {
+      throw new Error(error);
+    }
+  });
   
 
-module.exports={createCoupon,getALLCoupon,getACoupon}
+module.exports={createCoupon,getALLCoupon,getACoupon,updateCoupon,deleteCoupon}
--- routes/CouponRoute.js
+++ routes/CouponRoute.js
@@ -1,12 +1,14 @@
 const express=require("express")
 const { authMiddleware, isAdmin } = require('../middlewares/autMiddlewares');
-const { createCoupon, getALLCoupon, getACoupon } = require("../controller/CouponCtrl");
+const { createCoupon, getALLCoupon, getACoupon,deleteCoupon ,updateCoupon} = require("../controller/CouponCtrl");
 
 
 const router=express.Router();
 
 router.post('/',authMiddleware,isAdmin,createCoupon) 
 router.get('/',authMiddleware,isAdmin,getALLCoupon) 
-router.get('/:id',authMiddleware,isAdmin,getACoupon) 
+router.get('/:id',authMiddleware,isAdmin,getACoupon)
+router.put('/:id',authMiddleware,isAdmin,updateCoupon)
+router.delete('/:id',authMiddleware,isAdmin,deleteCoupon)
 
 module.exports=router
