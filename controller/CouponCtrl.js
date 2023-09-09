1c87e989094ee1d1df181abe380c7db0c5e42621
ismailahammed1 <ismailahmmedroman@gmail.com>
2023-09-10 05:07:34 +0600

    create coupon get all

--- controller/CouponCtrl.js
+++ controller/CouponCtrl.js
@@ -11,6 +11,23 @@ const createCoupon = asyncHandler(async (req, res) => {
       throw new Error(error);
     }
   });
+const getALLCoupon = asyncHandler(async (req, res) => {
+    try {
+      const findAllCoupon = await Coupon.find();
+      res.status(201).json(findAllCoupon);
+    } catch (error) {
+      throw new Error(error);
+    }
+  });
+const getACoupon = asyncHandler(async (req, res) => {
+    try {
+      const {id}=req.params
+      const findACoupon = await Coupon.findById(id);
+      res.status(201).json(findACoupon);
+    } catch (error) {
+      throw new Error(error);
+    }
+  });
   
 
-module.exports={createCoupon,}
+module.exports={createCoupon,getALLCoupon,getACoupon}
--- routes/CouponRoute.js
+++ routes/CouponRoute.js
@@ -1,10 +1,11 @@
 const express=require("express")
 const { authMiddleware, isAdmin } = require('../middlewares/autMiddlewares');
-const { createCoupon } = require("../controller/CouponCtrl");
+const { createCoupon, getALLCoupon } = require("../controller/CouponCtrl");
 
 
 const router=express.Router();
 
-router.post('/',authMiddleware,isAdmin,createCoupon)
+router.post('/',authMiddleware,isAdmin,createCoupon) 
+router.get('/',authMiddleware,isAdmin,getALLCoupon) 
 
 module.exports=router
